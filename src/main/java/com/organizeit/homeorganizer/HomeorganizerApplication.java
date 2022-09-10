825d5dbe3697445cea96b05d9625d05f265adfb5
patryk <krajewskipatryk@onet.pl>
2022-09-10 17:21:31 +0200

    Initial commit

---
+++ src/main/java/com/organizeit/homeorganizer/HomeorganizerApplication.java
@@ -0,0 +1,13 @@
+package com.organizeit.homeorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeorganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/organizeit/homeorganizer/HomeorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizeit.homeorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
